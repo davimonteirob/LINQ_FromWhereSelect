daa7043c30e15d732fa0a9196adb107aedd3e91e
davi321522@gmail.com <davi321522@gmail.com>
2024-06-25 16:07:14 -0300

    corrigindo para Consulta LINQ

--- UsandoLINQ_FromWhereSelect/Program.cs
+++ UsandoLINQ_FromWhereSelect/Program.cs
@@ -2,6 +2,7 @@
 
 Produto produto = new Produto();
 
-//Crie a lista e produtos e faça uma consulta usando o LINQ
+//Crie a lista de produtos e faça uma consulta usando o LINQ
+//corrija o que for necessário
 List<Produto> produtos = new List<Produto>{Id = 0, Nome = "Lapis", Categoria = "Quinta", Preco = 140 };
 
