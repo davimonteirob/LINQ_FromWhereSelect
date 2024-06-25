7f0ed7ed0c32ad72984619e39fad4c91bcf0b719
davi321522@gmail.com <davi321522@gmail.com>
2024-06-25 16:04:01 -0300

    Usando o LINQ

---
+++ UsandoLINQ_FromWhereSelect/Program.cs
@@ -0,0 +1,7 @@
+﻿using UsandoLINQ_FromWhereSelect;
+
+Produto produto = new Produto();
+
+//Crie a lista e produtos e faça uma consulta usando o LINQ
+List<Produto> produtos = new List<Produto>{Id = 0, Nome = "Lapis", Categoria = "Quinta", Preco = 140 };
+
