0a16d7dc5e89bf798c962b462abfedf3bbe5c8fc
davi321522@gmail.com <davi321522@gmail.com>
2024-06-26 15:33:41 -0300

    new att 03

--- UsandoLINQ_FromWhereSelect/Program.cs
+++ UsandoLINQ_FromWhereSelect/Program.cs
@@ -13,12 +13,30 @@ List<Produto> produtos = new List<Produto>
 
 };
 
+
+var filterP = from p in produtos
+                        where p.Preco > 100
+                        select p;
+
+foreach (var p in filterP)
+{
+    Console.WriteLine($" {p.Nome}, {p.Preco}");
+}
+
+
+
+
+
+
+//-------------------------------------------------------------
 int numDeBusca = 15;
 //testando consulta LINQ em array do tipo int
 int[] numbers = {10, 15 , 20 , 25 , 30, 35 , 40, 45, 50};
-
+               //from: define a origem, qual fonte de dados iremos tratar.
 var numQuery = from num in numbers
+               //where: define uma condição que queremos filtrar com os dados que estamos recebendo.
                where num % 2 == 0
+               //select: selecionamos o que queremos como retorno
                select num;
 
 foreach (var num in numQuery)
