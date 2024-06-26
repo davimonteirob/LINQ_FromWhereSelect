92bbf9485471577108c71f527ec09a40b98b9c7f
davi321522@gmail.com <davi321522@gmail.com>
2024-06-26 14:28:12 -0300

    nova att 02

--- UsandoLINQ_FromWhereSelect/Program.cs
+++ UsandoLINQ_FromWhereSelect/Program.cs
@@ -1,8 +1,29 @@
 ﻿using UsandoLINQ_FromWhereSelect;
 
-Produto produto = new Produto();
+
 Console.WriteLine("gg");
 //Crie a lista de produtos e faça uma consulta usando o LINQ
 //corrija o que for necessário
-List<Produto> produtos = new List<Produto>{Id = 0, Nome = "Lapis", Categoria = "Quinta", Preco = 140 };
+List<Produto> produtos = new List<Produto>
+{
+    new Produto(321, "Maquineta", "Média", 110 ),
+    new Produto(322, "Martelo", "Média", 150 ),
+    new Produto(323, "Chave De Fenda", "Baixa", 84),
+    new Produto(324, "Martelete", "Alta", 350)
+
+};
+
+int numDeBusca = 15;
+//testando consulta LINQ em array do tipo int
+int[] numbers = {10, 15 , 20 , 25 , 30, 35 , 40, 45, 50};
+
+var numQuery = from num in numbers
+               where num % 2 == 0
+               select num;
+
+foreach (var num in numQuery)
+{
+    Console.WriteLine(num);
+}
+
 
